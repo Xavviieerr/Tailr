d307af21243a8bb631be91b857e6082252f60772
Chidera paul ogbu <ogbupaul545@gmail.com>
2026-02-13 23:50:49 +0100

    successfully gets all cvs a user creates

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import cvReducer from "./allCvslice";
 
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
+		cvs: cvReducer,
 	},
 });
 
---
+++ client/src/types/cv.ts
@@ -0,0 +1,5 @@
+export interface CVState {
+	cvs: any[];
+	loading: boolean;
+	error: string | null;
+}
